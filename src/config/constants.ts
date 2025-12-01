4c10b3e54c83c4ec100b5f99acba9ad8ecab9701
Deepak Sapkota <sapkotad64@gmail.com>
2025-12-01 17:51:12 +0545

    feat: add API configuration and constants

---
+++ src/config/constants.ts
@@ -0,0 +1,10 @@
+export const API_CONFIG = {
+  TODO_BASE_URL: 'http://localhost:3000/api',
+  CHAT_BASE_URL: 'http://localhost:4000/api',
+};
+
+export const ENDPOINTS = {
+  TODOS: '/todos',
+  THREADS: '/threads',
+  CHAT: '/chat',
+};
